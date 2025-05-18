3bfd15a69ccb4e8f05c18a9d21f9881f98248721
Jaydev Tonde <47071053+jaytonde@users.noreply.github.com>
2025-05-18 12:02:32 +0530

    Added multiplicative module

--- RNAModel.py
+++ RNAModel.py
@@ -12,6 +12,17 @@ from einops                   import rearrange, repeat, reduce
 from einops.layers.torch      import Rearrange
 
 
+def init_weights(m):
+    #print(m)
+    if m is not None and isinstance(m, nn.Linear):
+        pass
+        # torch.nn.init.xavier_uniform_(m.weight)
+        # #torch.nn.init.xavier_normal(m.bias)
+        # try:
+        #     m.bias.data.fill_(0.01)
+        # except:
+        #     pass
+
 class OuterProductMean(nn.Module):
     def __init__(self, in_dim=256, dim_msa=32, pairwise_dim=64):
         super(OuterProductMean, self).__init__()
@@ -149,6 +160,70 @@ class MultiHeadAttention(nn.Module):
         q = self.layer_norm(q)
 
         return q, attn
+
+class TriangleMultiplicativeModule(nn.Module):
+    def __init__(
+        self,
+        *,
+        dim,
+        hidden_dim = None,
+        mix = 'ingoing'
+    ):
+        super().__init__()
+        assert mix in {'ingoing', 'outgoing'}, 'mix must be either ingoing or outgoing'
+
+        hidden_dim = default(hidden_dim, dim)
+        self.norm = nn.LayerNorm(dim)
+
+        self.left_proj = nn.Linear(dim, hidden_dim)
+        self.right_proj = nn.Linear(dim, hidden_dim)
+
+        self.left_gate = nn.Linear(dim, hidden_dim)
+        self.right_gate = nn.Linear(dim, hidden_dim)
+        self.out_gate = nn.Linear(dim, hidden_dim)
+
+        # initialize all gating to be identity
+
+        for gate in (self.left_gate, self.right_gate, self.out_gate):
+            nn.init.constant_(gate.weight, 0.)
+            nn.init.constant_(gate.bias, 1.)
+
+        if mix == 'outgoing':
+            self.mix_einsum_eq = '... i k d, ... j k d -> ... i j d'
+        elif mix == 'ingoing':
+            self.mix_einsum_eq = '... k j d, ... k i d -> ... i j d'
+
+        self.to_out_norm = nn.LayerNorm(hidden_dim)
+        self.to_out = nn.Linear(hidden_dim, dim)
+
+    def forward(self, x, src_mask):
+        src_mask=src_mask.unsqueeze(-1).float()
+        mask = torch.matmul(src_mask,src_mask.permute(0,2,1))
+        assert x.shape[1] == x.shape[2], 'feature map must be symmetrical'
+        if exists(mask):
+            mask = rearrange(mask, 'b i j -> b i j ()')
+
+        x = self.norm(x)
+
+        left = self.left_proj(x)
+        right = self.right_proj(x)
+
+        if exists(mask):
+            left = left * mask
+            right = right * mask
+
+        left_gate = self.left_gate(x).sigmoid()
+        right_gate = self.right_gate(x).sigmoid()
+        out_gate = self.out_gate(x).sigmoid()
+
+        left = left * left_gate
+        right = right * right_gate
+
+        out = einsum(self.mix_einsum_eq, left, right)
+
+        out = self.to_out_norm(out)
+        out = out * out_gate
+        return self.to_out(out)
 
 class ConvTransformerEncoderLayer(nn.Module):
     def __init__(self, d_model, nhead, dim_feedforward, pairwise_dim, dropout=0.1, k=3):
